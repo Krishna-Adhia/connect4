3cd691047a1e6d7e81e4e8a3fb6aba27f3460908
krishna <krishnaadhia@gmail.com>
2022-03-07 18:56:05 +0530

    Red Four in a line vertical

--- states/gameplay.js
+++ states/gameplay.js
@@ -18,8 +18,8 @@ export default class GameplayState extends Phaser.State{
         this.board = this.game.add.sprite(20,10,'board');
         this.board.inputEnabled = true;
         //on click of board area call clickedd function
-        this.board.events.onInputDown.add(this.insertcoins,this);
-        this.board.events.onInputDown.add(this.checkfourcoins,this);
+        this.board.events.onInputDown.add(this.findcol,this);
+        //this.board.events.onInputDown.add(this.checkfourcoins,this);
         for(let i=0;i<=5;i++)
         {
             arr.push([]);
@@ -30,15 +30,12 @@ export default class GameplayState extends Phaser.State{
                 
             }
         }
-        console.log(arr);  
     }
 
 
     //function to insert coins
-    insertcoins()
+    findcol()
     {
-        let actualypos = 570;
-        
         //find out which column is clicked
         let xpos = game.input.x;
         let ypos = game.input.y;
@@ -51,86 +48,99 @@ export default class GameplayState extends Phaser.State{
                 break;
 
             case(xpos<=250):
-                colselected = 1;
+                this.colselected = 1;
                 actualxpos = 155;
                 break;
 
             case(xpos<=360):
-                colselected = 2;
+                this.colselected = 2;
                 actualxpos = 260;
                 break;
 
             case(xpos<=460):
-                colselected = 3;
+                this.colselected = 3;
                 actualxpos = 370;
                 break;
 
             case(xpos<=570):
-                colselected = 4;
+                this.colselected = 4;
                 actualxpos = 470;
                 break;
 
             case(xpos<=680):
-                colselected = 5;
+                this.colselected = 5;
                 actualxpos = 580;
                 break;
 
             case(xpos<=790):
-                colselected = 6;
+                this.colselected = 6;
                 actualxpos = 685;
                 break;
         }
-        console.log("actualxpos: "+actualxpos);
-        console.log("colselected: "+colselected);
+        //calling the function to insertcoin
+        this.insertcoin(this.colselected);
+    }
 
-        
-
+    
+    //taking the value of column from previous function
+     insertcoin(colselected)
+    {   
+        let actualypos = 570;     
         //inserting the coins
         for(let i=5;i>=0;i--)
         {    
-            console.log("actualypos: "+actualypos);
+            console.log(arr);  
+            //if the hole is empty then insert coin
             if(arr[i][colselected].coin === null)
             {
-                console.log("i: "+i);
+                console.log("row: "+i);
+                console.log("col: "+colselected);
+
+                //inserting red coin on even click
                 if(click%2==0)
                 {
                     arr[i][colselected].coin = this.game.add.sprite(actualxpos,actualypos,'redcircle');
                     arr[i][colselected].color = "red";
-                    console.log(i);  
                 }
+                //inserting blue coin on odd click
                 else
                 {
                     arr[i][colselected].coin = this.game.add.sprite(actualxpos,actualypos,'bluecircle');
                     arr[i][colselected].color = "blue";
-                    console.log(i);
                 }
                 arr[i][colselected].coin = 'inserted'; 
                 click++;
-                
                 break; 
-                
-            } else {
-                actualypos = actualypos-107;
             }
-        }    
+            //else decreament the ypos to move up to the next hole 
+            else {
+                actualypos = actualypos-107;  
+            }
+        }
+        this.countfour(colselected); 
     }
 
-    //checking four in a line
-    checkfourcoins()
+    //checking for four in a line
+    countfour(colselected)
     {
+        let verticalcounter = 0;
+        //let counterforcoin=0;
+        for(let i=5;i>=0;i--)
+        {
+            if(arr[i][colselected].coin === 'inserted' && arr[i][colselected].color === 'red')
+            {
+                verticalcounter++;
+            }   
+            else
+            {
+                verticalcounter =0;
+            }
+            if(verticalcounter === 4)
+            {
+                console.log("wins");
+            }
+        }
         
-    //    for(let i=5;i>=2;i--)
-    //    {
-    //     if(arr[i][colselected].coin !== null)
-    //     {
-    //         counter++;
-    //     }
-    //    }
-    //    //console.log(counter);
-    //    if(counter == 4)
-    //    {
-    //        console.log("wins");
-    //    }
     }
-
+   
 }
